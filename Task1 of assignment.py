287534177aa51b8666ced242fc54ad9885188a9f
jainulabdeen123 <jainulabdeensk123@gmail.com>
2026-01-29 06:18:57 +0530

    initial cpmmit of assignment tasks

---
+++ Task1 of assignment.py
@@ -0,0 +1,6 @@
+a=float(input("enter your first number"))
+b=float(input("enter your second number"))
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
